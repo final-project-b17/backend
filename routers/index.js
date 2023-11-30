1b3c3c728033812040d3f521456dde4ea50733b8
amarafiif <amarafif230@gmail.com>
2023-12-01 01:45:10 +0700

    optimize: chapter endpoints

--- routers/index.js
+++ routers/index.js
@@ -2,15 +2,10 @@ const express = require("express"),
 	router = express.Router(),
 	authRouter = require("./auth.routers"),
 	categoryRouter = require("./category.router"),
-	courseRouter = require("./course.router"),
-	chapterRouter = require("./chapter.router");
-
-// paginationRouter = require("./pagination.router");
-// const authenticateUser = require("../middlewares/authentication");
+	courseRouter = require("./course.router");
 
 router.use("/auth", authRouter);
-router.use("/courses", courseRouter);
 router.use("/categories", categoryRouter);
-router.use("/chapters", chapterRouter);
+router.use("/courses", courseRouter);
 
 module.exports = router;
