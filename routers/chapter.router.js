665b5c12447b213b3e515e06957d41f4070deefd
amarafiif <amarafif230@gmail.com>
2023-12-03 23:02:10 +0700

    fix: docs-api and endpoints

--- routers/chapter.router.js
+++ routers/chapter.router.js
@@ -2,8 +2,8 @@ const express = require("express"),
 	router = express.Router(),
 	controller = require("../controllers/chapter.controller");
 
-router.post("/create", controller.createChapter);
 router.get("/", controller.getChapters);
+router.post("/create", controller.createChapter);
 router.get("/:id", controller.getChapterId);
 router.put("/update/:id", controller.updateChapterId);
 router.delete("/delete/:id", controller.deleteChapterId);
--- routers/course.router.js
+++ routers/course.router.js
@@ -3,8 +3,8 @@ const express = require("express"),
 	courseController = require("../controllers/course.controller");
 
 router.get("/", courseController.listCourse);
-router.post("/add-course", courseController.createCourse);
-router.get("/detail-course/:id", courseController.detailCourse);
+router.post("/create", courseController.createCourse);
+router.get("/detail/:id", courseController.detailCourse);
 router.put("/update/:id", courseController.updateCourse);
 router.delete("/delete/:id", courseController.deleteCourseById);
 
