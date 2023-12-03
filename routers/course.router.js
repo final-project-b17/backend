91d38cc0148222dea973aea773a3c3e04b0823f8
amarafiif <amarafif230@gmail.com>
2023-12-03 14:48:55 +0700

    materials: optimize endpoints

--- routers/course.router.js
+++ routers/course.router.js
@@ -1,7 +1,8 @@
 const express = require("express"),
 	router = express.Router(),
 	courseController = require("../controllers/course.controller"),
-	chapterController = require("../controllers/chapter.controller");
+	chapterController = require("../controllers/chapter.controller"),
+	MaterialsController = require("../controllers/courseMaterial.controller");
 
 // /courses
 router.get("/", courseController.listCourse);
@@ -13,5 +14,12 @@ router.post("/chapters/create", chapterController.createChapter);
 router.get("/chapters/:id", chapterController.getChapterId);
 router.put("/chapters/update/:id", chapterController.updateChapterId);
 router.delete("/chapters/delete/:id", chapterController.deleteChapterId);
+
+// /courses/chapter/course-materials
+router.get("/chapters/:id/course-materials/", MaterialsController.listCourseMaterials);
+router.post("/chapters/:id/course-materials/create", MaterialsController.createCourseMaterials);
+router.get("/chapters/course-materials/:id", MaterialsController.listCourseMaterialsId);
+router.put("/chapters/course-materials/update", MaterialsController.updateCourseMaterialsId);
+router.put("/chapters/course-materials/delete", MaterialsController.deleteCourseMaterialsId);
 
 module.exports = router;
