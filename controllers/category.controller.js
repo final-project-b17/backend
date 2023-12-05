62195e3724aa09de1f87c40346572011affec374
Nouva Rp <nouvarizqy@gmail.com>
2023-12-05 15:16:46 +0700

    add: list, update, delete

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -30,23 +30,100 @@ module.exports = {
 	// Display list category
 	listCategories: async (req, res) => {
 		try {
-			const data = await categories.findMany();
+			const listCategory = await categories.findMany();
 
-			res.status(200).json({
+			res.json({
 				success: true,
-				categories: data,
+				data: listCategory,
 			});
-
 		} catch (error) {
-			console.log(error);
+			console.error("Error retrieving Category:", error);
 			res.status(500).json({
-				success: false,
-				error: "Internal Server Error",
+			  success: false,
+			  error: "Internal Server Error",
 			});
 		}
 	},
 
-	// Edit category by id
+	// Display list category id
+	listCategoriesId: async (req, res) => {
+		try {
+		  const listCategoryId = parseInt(req.params.id);
+		  const listCategory = await categories.findUnique({
+			where: {
+			  id: listCategoryId,
+			},
+			include: {
+				courses: true,
+			},
+		  });
+	
+		  if (!listCategory) {
+			return res.status(404).json({
+			  success: false,
+			  message: "Category not found",
+			});
+		  }
+	
+		  res.json({
+			success: true,
+			data: listCategory,
+		  });
+		} catch (error) {
+		  console.error("Error retrieving Category: ", error);
+		  res.status(500).json({
+			success: false,
+			error: "Internal Server Error",
+		  });
+		}
+	  },
 
+	// Edit category by id
+    updateCategory: async (req, res) => {
+		try {
+		  const categoryId = parseInt(req.params.id);
+		  const updateCategory = await categories.update({
+			where: {
+			  id: categoryId,
+			},
+			data: {
+				title: req.body.title,
+			},
+		  });
+	
+		  res.json({
+			success: true,
+			data: updateCategory,
+		  });
+		} catch (error) {
+		  console.log(error);
+		  res.status(500).json({
+			success: false,
+			error: "Internal Server Error",
+		  });
+		}
+	  },
+  
 	// Delete Category
+	deleteCategory: async (req, res) => {
+		try {
+		  const categoryId = parseInt(req.params.id);
+		  await categories.delete({
+			where: {
+			  id: categoryId,
+			},
+		  });
+	
+		  res.json({
+			success: true,
+			message: "Category deleted successfully",
+		  });
+		} catch (error) {
+		  console.log(error);
+		  res.status(500).json({
+			success: false,
+			error: "Internal Server Error",
+		  });
+		}
+	  },
 };
