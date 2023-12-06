74a8dbca7b956e9b8c2fc15545712a6db0a3948b
amarafiif <amarafif230@gmail.com>
2023-12-06 11:40:27 +0700

    feat: detail, update, delete category

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -17,7 +17,6 @@ module.exports = {
 				message: "Created!",
 				categories: data,
 			});
-      
 		} catch (error) {
 			console.log(error);
 			res.status(500).json({
@@ -36,7 +35,6 @@ module.exports = {
 				success: true,
 				categories: data,
 			});
-
 		} catch (error) {
 			console.log(error);
 			res.status(500).json({
@@ -46,7 +44,82 @@ module.exports = {
 		}
 	},
 
+	// Detail Category by id
+	detailCategory: async (req, res) => {
+		try {
+			const listCategoryId = parseInt(req.params.id);
+			const listCategory = await categories.findUnique({
+				where: {
+					id: listCategoryId,
+				},
+			});
+
+			if (!listCategory) {
+				return res.status(404).json({
+					success: false,
+					message: "Category not found",
+				});
+			}
+
+			res.json({
+				success: true,
+				categories: listCategory,
+			});
+		} catch (error) {
+			console.error("Error retrieving Category: ", error);
+			res.status(500).json({
+				success: false,
+				error: "Internal Server Error",
+			});
+		}
+	},
+
 	// Edit category by id
+	updateCategory: async (req, res) => {
+		try {
+			const categoryId = parseInt(req.params.id);
+			const updateCategory = await categories.update({
+				where: {
+					id: categoryId,
+				},
+				data: {
+					title: req.body.title,
+				},
+			});
+
+			res.json({
+				success: true,
+				data: updateCategory,
+			});
+		} catch (error) {
+			console.log(error);
+			res.status(500).json({
+				success: false,
+				error: "Internal Server Error",
+			});
+		}
+	},
 
 	// Delete Category
+	deleteCategory: async (req, res) => {
+		try {
+			const categoryId = parseInt(req.params.id);
+			await categories.delete({
+				where: {
+					id: categoryId,
+				},
+			});
+
+			res.json({
+				success: true,
+				message: "Category deleted successfully",
+			});
+		} catch (error) {
+			console.log(error);
+			res.status(500).json({
+				success: false,
+				error: "Internal Server Error",
+			});
+		}
+	},
 };
