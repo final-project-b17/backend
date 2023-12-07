0013d0cb5eb927514cbe6046b540806f37738a8c
21081010047AdeliaPutriAdyani <21081010047@student.upnjatim.ac.id>
2023-12-07 19:23:41 +0700

    update controller n router create and update with thumbnail

--- routers/category.router.js
+++ routers/category.router.js
@@ -1,11 +1,13 @@
 const express = require("express"),
 	router = express.Router(),
+	multer = require("../middlewares/multer"),
+	multerLib = require('multer')(),
 	controller = require("../controllers/category.controller");
 
 router.get("/", controller.listCategories);
-router.post("/create", controller.createCategory);
+router.post("/create", multerLib.single('thumbnail'), controller.createCategory);
 router.get("/detail/:id", controller.detailCategory);
-router.put("/update/:id", controller.updateCategory);
+router.put("/update/:id", multerLib.single('thumbnail'), controller.updateCategory);
 router.delete("/delete/:id", controller.deleteCategory);
 
 module.exports = router;
