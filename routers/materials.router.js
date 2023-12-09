ceff0bdd5c2e2c04239fdcb33e75cd44f39f730f
Muhammad 'Ammar Afif <89902158+amarafiif@users.noreply.github.com>
2023-12-09 07:50:59 +0700

    minorFix: route method materialCourse

--- routers/materials.router.js
+++ routers/materials.router.js
@@ -6,6 +6,6 @@ router.post("/create", materialController.createCourseMaterials);
 router.get("/", materialController.listCourseMaterials);
 router.get("/:id", materialController.listCourseMaterialsId);
 router.put("/update/:id", materialController.updateCourseMaterialsId);
-router.put("/delete/:id", materialController.deleteCourseMaterialsId);
+router.delete("/delete/:id", materialController.deleteCourseMaterialsId);
 
 module.exports = router;
