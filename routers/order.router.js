a80f56116e8b742b607c6fb74e8959a9b99b72d6
amarafiif <amarafif230@gmail.com>
2024-01-05 11:29:51 +0700

    fix: callback xendit to update status payment

--- routers/order.router.js
+++ routers/order.router.js
@@ -3,7 +3,8 @@ const orderController = require("../controllers/order.controller");
 const authenticateUser = require("../middlewares/authentication");
 const router = express.Router();
 
-// POST /orders
 router.post("/create", authenticateUser, orderController.createOrder);
+router.get("/history", authenticateUser, orderController.myOrder);
+router.post("/callback", orderController.xenditCallback);
 
 module.exports = router;
